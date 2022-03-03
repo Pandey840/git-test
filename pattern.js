de87d072cd85a0fe120fba2e577f5451e0c855a3
Pandey840 <dhirajpandey37@gmail.com>
2022-03-03 16:12:36 +0530

    first changes

---
+++ pattern.js
@@ -0,0 +1,40 @@
+// 1. Pattern 
+
+// for (let i=1; i<6; i++)
+// { console.log("* * * * *")}
+
+
+
+
+// 2. Pattern
+
+// for (let i=1 ; i<6 ; i++)
+// { console.log(i, i, i, i, i)}
+
+
+
+
+// 3. Pattern
+
+// for (let i=1, j=1; j<6; j++)
+// {console.log(i, i+1, i+2, i+3, i+4)}
+
+
+
+
+// 4. Pattern
+
+// for (let i=65; i<70; i++)
+// {console.log(String.fromCharCode(i, i, i, i, i))}
+
+
+
+
+// 5. Pattern
+
+// for (let i=65, j=1; j<6; j++ )
+// { console.log(String.fromCharCode(i, i+1, i+2, i+3, i+4))}
+
+
+
+
